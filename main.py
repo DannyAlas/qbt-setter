800e13af0c1c1670d3c80f5dcc085678583631ce
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2024-02-03 00:01:52 -0700

    better errors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import sys
 import time
 
 import requests
@@ -11,7 +12,7 @@ logging.basicConfig(
 )
 
 log = logging.getLogger(__name__)
-qbt_url = "http://10.32.32.20:8080"
+qbt_url = os.getenv("QBT_URL", "http://localhost:8080")
 
 
 def main():
@@ -36,36 +37,61 @@ This script will keep changing it back to eth0
     while True:
         try:
             login_payload = {
-                "username": os.environ["QBT_USERNAME"],
-                "password": os.environ["QBT_PASS"],
+                "username": os.getenv("QBT_USERNAME", None),
+                "password": os.getenv("QBT_PASS", None),
             }
-            login_headers = {"Referer": qbt_url}
-            login_response = requests.post(
-                qbt_url + "/api/v2/auth/login",
-                data=login_payload,
-                headers=login_headers,
-            )
-            cookie = "SID=" + str(login_response.cookies.get_dict()["SID"])
-
-            settings = requests.get(
-                qbt_url + "/api/v2/app/preferences", headers={"Cookie": cookie}
-            )
-            settings_json = settings.json()
-            if settings_json.get("current_network_interface") != "eth0":
-                json_data = {"current_network_interface": "eth0"}
-                requests.put(
-                    qbt_url + "/api/v2/app/preferences",
-                    json=json.dumps(json_data),
-                    headers={"Cookie": cookie},
+            if not login_payload["username"] or not login_payload["password"]:
+                err = (
+                    "QBT_USERNAME Not set"
+                    if not login_payload["username"]
+                    else "QBT_PASS Not set"
                 )
-                log.info("Changed network interface to eth0")
-                interval = 240
+                log.error(err)
+                sys.exit(1)
+            login_headers = {"Referer": qbt_url}
+            try:
+                login_response = requests.post(
+                    qbt_url + "/api/v2/auth/login",
+                    data=login_payload,
+                    headers=login_headers,
+                )
+                cookie = "SID=" + str(login_response.cookies.get_dict()["SID"])
+            except Exception as e:
+                log.error(f"Authentication failed: {e}")
+                interval = 20
+                time.sleep(interval)
+                continue
+            try:
+                settings = requests.get(
+                    qbt_url + "/api/v2/app/preferences", headers={"Cookie": cookie}
+                )
+
+                settings_json = settings.json()
+                cni = settings_json["current_network_interface"]
+            except Exception as e:
+                log.error(f"Failed to get settings: {e}")
+                interval = 20
+                time.sleep(interval)
+                continue
+            if cni != "eth0":
+                try:
+                    json_data = {"current_network_interface": "eth0"}
+                    requests.put(
+                        qbt_url + "/api/v2/app/preferences",
+                        json=json.dumps(json_data),
+                        headers={"Cookie": cookie},
+                    )
+                    log.info("Changed network interface to eth0")
+                    interval = 240
+                except Exception as e:
+                    log.error(f"Failed to change network interface: {e}")
+                    interval = 20
             else:
                 log.info("Network interface is already set to eth0")
                 interval = 240
             time.sleep(interval)
         except Exception as e:
-            log.error(f"Error: {e}")
+            log.error(f"An Unknown error occurred: {e}")
             interval = 20
             time.sleep(interval)
 
